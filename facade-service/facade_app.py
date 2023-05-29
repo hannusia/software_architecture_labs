04765c420b71672e2369b9a4e5b1d470b5416c1d
hannusia <hanna.yershova@ucu.edu.ua>
2023-05-30 01:09:24 +0300

    add lab files

---
+++ facade-service/facade_app.py
@@ -0,0 +1,69 @@
+#!flask/bin/python
+
+from flask import Flask, request
+from requests import post, get
+import uuid
+import sys
+import random
+import socket
+import hazelcast
+
+logging_list = ['8000', '8001', '8002']
+messag_list = '8800,8801'
+messag_list = messag_list.split(',')
+
+clients = hazelcast.HazelcastClient
+
+client = hazelcast.HazelcastClient(
+    cluster_name="my-cluster"
+)
+
+my_queue = client.get_queue('my-queue').blocking()
+app = Flask(__name__)
+
+
+def check_port(port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    result = sock.connect_ex(('127.0.0.1', int(port)))
+    sock.close()
+    return result == 0
+
+
+@app.route('/facade', methods=['GET', 'POST'])
+def requests():
+    logg_random = random.choice(logging_list)
+    if not check_port(logg_random):
+        while not check_port(logg_random):
+            logging_list.remove(logg_random)
+            logg_random = random.choice(logging_list)
+    logging_url = "http://127.0.0.1:" + logg_random + "/logging"
+
+    messag_random = random.choice(messag_list)
+    if not check_port(messag_random):
+        while not check_port(messag_random):
+            messag_list.remove(messag_random)
+            messag_random = random.choice(messag_list)
+    messg_url = "http://127.0.0.1:" + messag_random + "/messages"
+
+    if request.method == 'GET':
+        r_l = get(logging_url).content.decode("utf-8")
+        r_m = get(messg_url).content.decode("utf-8")
+        final_string = "Logging-service response: " + '\n' + r_l + "Messages-service response: " + r_m + '\n'
+        return final_string
+
+    if request.method == 'POST':
+        mssg = request.get_data()
+        my_queue.put(mssg)
+
+        # generate random UUID
+        mssg_uuid = uuid.uuid4().hex  # for a little shorter and more readable version convert uuid to a 32-character
+        # hexadecimal string
+
+        r = post(logging_url, data={mssg_uuid: mssg})
+        r2 = post(messg_url)
+
+        return mssg
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8888)
